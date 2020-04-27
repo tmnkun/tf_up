86f3718e0e830a81ad9c2f53995a2cb9134678e0
Denis Slepov <tmnkun@gmail.com>
2020-04-28 01:13:20 +0500

    add http server

--- main.tf
+++ main.tf
@@ -3,11 +3,39 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-resource "aws_instance" "aws_instance_example" {
+resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instans.id]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
   tags = {
-      Name = "terraform-ex"
+      Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instans" { 
+  name = "terraform-example-instans"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP addressof the web server"
 }
